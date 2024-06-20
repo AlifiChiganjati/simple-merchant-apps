2e3e2ccdf3e6995362892e99ae2de46c1033ef55
Alifi Chiganjati <alifichiganjati@gmail.com>
2024-06-20 14:33:43 +0700

    feat: add .env in .gitignore and add ddl sql

---
+++ assets/db/script.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE db_merchant;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS "users" (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  fullname VARCHAR(30) NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  phone_number VARCHAR(15) NOT NULL,
+  point INTEGER DEFAULT 0 NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "merchants" (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID NOT NULL REFERENCES users(id),
+  merchant_name VARCHAR(30) NOT NULL,
+  merchant_address VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "products" (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  merchant_id UUID NOT NULL REFERENCES merchants(id),
+  name VARCHAR(30) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  point INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "transactions" (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID NOT NULL REFERENCES users(id),
+  merchant_id UUID NOT NULL REFERENCES merchants(id),
+  transaction_noT INTEGER NOT NULL,
+  point_total INTEGER NOT NULL,
+  subtotal DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "transactions_detail" (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  transaction_id UUID NOT NULL REFERENCES transactions(id),
+  product_id UUID NOT NULL REFERENCES products(id),
+  qty INTEGER NOT NULL,
+  point INTEGER NOT NULL,
+  total DECIMAL(10,2) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
